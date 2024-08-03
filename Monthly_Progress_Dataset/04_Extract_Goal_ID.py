18f1cf8c66f6379ec1ce19acd14c1ba5b6f6615c
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-08-03 15:40:26 +0700

    Update_Extract_Goal_ID

--- Monthly_Progress_Dataset/04_Extract_Goal_ID.py
+++ Monthly_Progress_Dataset/04_Extract_Goal_ID.py
@@ -5,6 +5,12 @@ def extract_goal_id(text):
 # def extract_patterns(text):
 #     return re.findall(r'\b\d+\.\d*\b', text)
 
+def remove_goal_id(text):
+    return re.sub(r'\d+\.\d?', '', text).strip()
+
+
 df_progress['No. (Goal)'] = df_progress['Goal'].apply(extract_goal_id)
 df_progress['No. (Goal)'] = df_progress['No. (Goal)'].replace('', np.nan)
 df_progress['No. (Goal)'] = df_progress['No. (Goal)'].str.strip('.')
+
+df_progress['Goal'] = df_progress['Goal'].apply(lambda x: remove_goal_id(x))
