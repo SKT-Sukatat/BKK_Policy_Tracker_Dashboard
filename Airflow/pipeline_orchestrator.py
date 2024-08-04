3dcf76fdbd192d6b4d8b74dc58a96daff37736a0
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-08-04 10:58:33 +0700

    Init_Pipeline_Orchestrator

---
+++ Airflow/pipeline_orchestrator.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner':'User',
+    'email':['skt.sukatat@gmail.com']
+}
+
+dag = DAG('test', catchup=False, default_args = default_args)
+
+# t1 = PythonOperator()
+
+# t2 - PythonOperator()
+
+# t1 >> t2
